55927f7fd81621971037c2489486727573e53515
MuhammadAbsarSiddiqui <128141992+MuhammadAbsarSiddiqui@users.noreply.github.com>
2024-01-27 20:32:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// ******QUESTION1******
+
+
+
+let number = prompt("ENTER THE NUMBER:");
+console.log("******QUESTION1******");
+console.log("\n");
+for(let i = 1; i <= 10 ; i++){
+    table = (i * number);
+    console.log(table);
+}
+
+
+// ******QUESTION2******"
+console.log("******QUESTION2******");
+let Num_fac=5;
+let factorial = 1;
+let j = 1;
+while(j <= Num_fac)
+{
+    factorial *= j;
+    j++;
+
+}
+console.log("\nThe factorial of 5 is:",factorial);
